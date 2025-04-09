3cd75e3bd553b02347b65cdb42b73a085ffccc96
MineMineMine221 <114082225+MineMineMine221@users.noreply.github.com>
2025-04-09 11:44:52 +0700

    UI Blink

---
+++ Assets/Scripts/UI/UIBlink.cs
@@ -0,0 +1,46 @@
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class UIBlink : MonoBehaviour
+{
+    [SerializeField] private Image uiGraphic;
+    [SerializeField] private float blinkSpeed = 1f;
+    private bool canPlay = true;
+    private Coroutine coroutine;
+    void Start()
+    {
+
+    //    StartCoroutine(FadeAlpha());
+    }
+    public void StopUiFade()
+    {
+        StopCoroutine(coroutine);
+    }
+    public void StartFade()
+    {
+        coroutine = StartCoroutine(FadeAlpha());
+    }
+    IEnumerator FadeAlpha()
+    {
+        while (true)
+        {
+
+            for (float i = 1f; i >= 0f; i -= Time.deltaTime * blinkSpeed)
+            {
+                Color currentColor = uiGraphic.color;
+                currentColor.a = i;
+                uiGraphic.color = currentColor;
+                yield return null;
+            }
+
+            for (float i = 0f; i <= 1f; i += Time.deltaTime * blinkSpeed)
+            {
+                Color currentColor = uiGraphic.color;
+                currentColor.a = i;
+                uiGraphic.color = currentColor;
+                yield return null;
+            }
+        }
+    }
+}
