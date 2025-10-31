e6325eb7222a59a52639bb6046fcda8bfdfd542f
MineMineMine221 <114082225+MineMineMine221@users.noreply.github.com>
2025-10-31 12:10:44 +0700

    add debug event

--- Assets/Scripts/GameEvent/GameEvent.cs
+++ Assets/Scripts/GameEvent/GameEvent.cs
@@ -9,35 +9,40 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Events/Game Event")]
 public class GameEvent : ScriptableObject
 {
-    [SerializeField] [TextArea] private string description;
+    [SerializeField][TextArea] private string description;
     public List<GameEventListener> listeners = new List<GameEventListener>();
 
-    public void Raise(Component _sender,object _data)
+    public void Raise(Component _sender, object _data)
     {
-           foreach (var t in listeners)
-           {
-               t.OnEventRised(_sender, _data);
-           }
+        if (EventSetting.showDebugRaiseEvent)
+        {
+            Debug.Log($"Rise Event name {this.name} at {_sender.gameObject.name}");
+
+        }
+        foreach (var t in listeners)
+        {
+            t.OnEventRised(_sender, _data);
+        }
 
     }
 
     public void RegisterListener(GameEventListener _gameEventListener)
     {
-      if(!listeners.Contains(_gameEventListener))
-          {
-              listeners.Add(_gameEventListener);
-          }
-       
+        if (!listeners.Contains(_gameEventListener))
+        {
+            listeners.Add(_gameEventListener);
+        }
+
     }
 
     public void UnregisterListener(GameEventListener _gameEventListener)
     {
-         if (listeners.Contains(_gameEventListener))
-          {
-              listeners.Remove(_gameEventListener);
-          }  
+        if (listeners.Contains(_gameEventListener))
+        {
+            listeners.Remove(_gameEventListener);
+        }
 
-     
+
     }
 
 
